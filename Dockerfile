ac309c914bf1a0568cecccb9db065e767844955f
ndittren <36773036+ndittren@users.noreply.github.com>
2023-10-24 15:51:18 -0400

    add docker for development

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.11
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+WORKDIR /src
+COPY requirements.txt /src/
+RUN apt update
+RUN apt install -y npm
+RUN pip install --no-deps -r requirements.txt
+COPY . /src/
---
+++ metricsmentor/settings_docker.py
@@ -0,0 +1,21 @@
+import os
+from metricsmentor.settings_shared import *  # noqa: F401,F403
+
+
+# docker-compose db container
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+
+try:
+    from metricsmentor.local_settings import *  # noqa: F401,F403
+except ImportError:
+    pass
