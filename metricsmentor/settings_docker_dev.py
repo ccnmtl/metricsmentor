8e6ef5325d2bbc283858332de4c90c72134ada25
ndittren <36773036+ndittren@users.noreply.github.com>
2024-03-07 12:29:04 -0500

    fix lint errors

--- metricsmentor/settings_docker_dev.py
+++ metricsmentor/settings_docker_dev.py
@@ -9,8 +9,11 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        # 'HOST': 'host.docker.internal', # use this for Mac and Windows when the Postgres is running in another Docker container outside the network
-        'HOST': 'db', # use this for Linux when the Postgres is running in another Docker container inside the network
+        # 'HOST': 'host.docker.internal',
+        # # use this for Mac and Windows when the Postgres
+        # is running in another Docker container outside the network
+        'HOST': 'db',  # use this for Linux when the Postgres is running
+                       # in another Docker container inside the network
         'PORT': 5432,
     }
 }
