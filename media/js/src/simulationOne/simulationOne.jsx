a782e5ddcc60d1b9f0778b02f6506389857b19c4
Zarina Mustapha <zarina@columbia.edu>
2024-04-15 15:42:51 -0400

    Apply styling to coefficient section

--- media/js/src/simulationOne/simulationOne.jsx
+++ media/js/src/simulationOne/simulationOne.jsx
@@ -199,43 +199,58 @@ export const SimulationOne = () => {
                             <div className='simulation__step-body'>
                                 <header className='simulation__step-header'>
                                     <h2 className='h2-primary'>
-                                        Graph Coefficients</h2>
+                                        Resulting Graph Coefficients</h2>
                                 </header>
                                 <div className='simulation__step-content'>
-                                    <div className='row'>
-                                        <Katex tex={
-                                            // eslint-disable-next-line max-len
-                                            'y = \\hat{\\beta_0} + \\hat{\\beta_1}x'
-                                        } />
-                                    </div>
-                                    <div className='row'>
+                                    <p>
+                                    The graph generated with your sample size
+                                    and correlation coefficient produces the
+                                    following coefficients. Let&rsquo;s look at
+                                    them in detail.
+                                    </p>
+                                    <h2 className='mt-4'>
+                                        Regression line equation:</h2>
+                                    <div className='ms-3 mb-3'>
                                         <Katex tex={
                                         // eslint-disable-next-line max-len
-                                            `y = ${intercept.toFixed(2)} + ${slope.toFixed(3)}x`
+                                            '\\hat{y} = \\hat{\\beta_0} + \\hat{\\beta_1}x'
                                         } />
                                     </div>
-                                    <div className='row'>
-                                        <label>
-                                            <Katex tex={
-                                            // eslint-disable-next-line max-len
-                                                `\\hat{\\beta_0} = ${intercept.toFixed(3)}`
-                                            } />
-                                        </label>
+                                    <div className='ms-3 mb-3'>
+                                        <Katex tex={
+                                        // eslint-disable-next-line max-len
+                                            `\\hat{y} = ${intercept.toFixed(2)} + ${slope.toFixed(3)}x`
+                                        } />
                                     </div>
-                                    <div className='row'>
-                                        <label>
-                                            <Katex tex={
-                                            // eslint-disable-next-line max-len
-                                                `\\hat{\\beta_1} = ${slope.toFixed(3)}`
-                                            } />
-                                        </label>
+                                    <p>where <span className='katex'>
+                                        <span className='mathnormal'>y</span> {/* eslint-disable-line max-len */}
+                                    </span> is the dependent variable and
+                                    <span className='katex'> <span className='mathnormal'>x</span> {/* eslint-disable-line max-len */}
+                                    </span> is the independent, or
+                                        explanatory, variable.
+                                    </p>
+                                    <h2 className='mt-4'>Sample <span className='katex'> <span className='mathnormal'>y</span> {/* eslint-disable-line max-len */}
+                                    </span>-intercept coefficient:</h2>
+                                    <div className='ms-3'>
+                                        <Katex tex={
+                                        // eslint-disable-next-line max-len
+                                            `\\hat{\\beta_0} = ${intercept.toFixed(3)}`
+                                        } />
                                     </div>
-                                    <div className='row'>
-                                        <label>
-                                            <Katex tex={
-                                            // eslint-disable-next-line max-len
-                                                `{SE} = ${stderror.toFixed(3)}`} />
-                                        </label>
+                                    <h2 className='mt-4'>Sample slope
+                                        coefficient:</h2>
+                                    <div className='ms-3'>
+                                        <Katex tex={
+                                        // eslint-disable-next-line max-len
+                                            `\\hat{\\beta_1} = ${slope.toFixed(3)}`
+                                        } />
+                                    </div>
+                                    <h2 className='mt-4'>Standard error of
+                                    the sample slope:</h2>
+                                    <div className='ms-3'>
+                                        <Katex tex={
+                                        // eslint-disable-next-line max-len
+                                            `{SE(\\hat{\\beta_1})} = ${stderror.toFixed(3)}`} />
                                     </div>
                                 </div>
                             </div>
