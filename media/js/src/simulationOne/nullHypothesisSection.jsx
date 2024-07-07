b591e33ba7f709f1a836efc2226514c5f6ea8dbd
ndittren <36773036+ndittren@users.noreply.github.com>
2024-07-07 18:11:16 -0400

    disable null hypothesis for now

--- media/js/src/simulationOne/nullHypothesisSection.jsx
+++ media/js/src/simulationOne/nullHypothesisSection.jsx
@@ -51,7 +51,8 @@ export const NullHypothesisSection = ({
                             <input size="10"
                                 className="form-control short-input"
                                 type="number" min="-5" max="5"
-                                disabled={startQuiz}
+                                // disabled={startQuiz}
+                                disabled={true}
                                 value={hypothesizedSlope}
                                 onChange={handleNullHypothesis} />
                         </div>
@@ -90,7 +91,8 @@ export const NullHypothesisSection = ({
                             <input size="10"
                                 className="form-control short-input"
                                 type="number" min="-5" max="5"
-                                disabled={startQuiz}
+                                // disabled={startQuiz}
+                                disabled={true}
                                 value={hypothesizedSlope}
                                 onChange={handleNullHypothesis} />
                         </div>
