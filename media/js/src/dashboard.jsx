a0097722d0ce457a038e95d50d7316a49ceb2985
Zarina Mustapha <zarina@columbia.edu>
2024-09-16 10:11:26 -0400

    Matching the sim1 and sim2 descriptions in dashboards and sim panels

--- media/js/src/dashboard.jsx
+++ media/js/src/dashboard.jsx
@@ -27,13 +27,15 @@ export const Dashboard = () => {
                                 {'Hypothesis Testing for Population Slope'}
                             </span>
                         </h2>
-                        <p>In this module, you will learn about
+                        <p>
+                            In this simulation, you&rsquo;ll learn about
                             hypothesis testing for the population slope
-                            in a simple linear regression model. You will
-                            be able to visualize the relationship between
-                            two variables and generate data to test the
-                            null hypothesis that the population slope is
-                            equal to a specified value.</p>
+                            in simple and multiple regression models.
+                            You&rsquo;ll be able to visualize the relationship
+                            between two, or three variables, and generate
+                            data to test the null hypothesis that the
+                            population slope is equal to a specified value.
+                        </p>
                         <Link to={`/course/${courseId}/simulations/1/`}
                             className="btn btn-primary my-3"
                             data-cy="sim-1-link">
@@ -54,7 +56,8 @@ export const Dashboard = () => {
                                 {'Omitted Variable Bias'}
                             </span>
                         </h2>
-                        <p>In this module, we will address the first
+                        <p>
+                            This simulation will address the first
                             endogeneity problem in regression analysis:
                             omitted variable bias (OVB). Overlooking certain
                             variables can lead to biased estimates of the
@@ -62,7 +65,8 @@ export const Dashboard = () => {
                             interactive exercises and examples, you&rsquo;ll
                             learn how to detect and correct for OVB in your
                             econometric analysis, ensuring that your findings
-                            are both reliable and valid.</p>
+                            are both reliable and valid.
+                        </p>
 
                         <Link to={`/course/${courseId}/simulations/2/`}
                             className="btn btn-primary my-3"
