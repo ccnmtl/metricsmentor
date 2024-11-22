2a768b9da121c6c332b94b55dcafe249b962cda6
Evan Petersen <emp2221@columbia.edu>
2024-11-22 15:58:02 -0500

    Initial Multicollinearity graph created

--- media/js/src/simulations/simulation3/simulationThree.jsx
+++ media/js/src/simulations/simulation3/simulationThree.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SkedasticityScatterPlot } from './skedasticityPlot';
+import { MulticollinearityScatterPlot } from './multicollinearityPlot';
+import DATA from './generatedData.json';
+
 
 export const SimulationThree = () => {
+
+    const [stage, setStage] = useState(0);
+
+    const handleStage = (e) => setStage(parseInt(e.target.value));
+
     return (
         <div className="simulation">
             <div className="simulation__workspace">
                 Work space
             </div>
             <div className="simulation__graphspace">
-                <SkedasticityScatterPlot initialSlope={2} initialIntercept={5}
-                    N={50} />
+                {['Skedasticity', 'Multicollinearity'].map((label, index) => (
+                    <button onClick={handleStage} key={index} value={index}
+                        className={'btn btn-light m-1'} disabled={stage===index}
+                    >
+                        {label}</button>))}
+                {stage === 0 && <SkedasticityScatterPlot initialSlope={2}
+                    initialIntercept={5} N={50} />}
+                {stage === 1 && <MulticollinearityScatterPlot data={DATA} />}
             </div>
         </div>
     );
