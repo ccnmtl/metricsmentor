a1507e7f05953adc304ddf417cd73aad53ec2f9a
ndittren <36773036+ndittren@users.noreply.github.com>
2025-03-18 16:49:41 -0400

    Filling out more of the real data portion

---
+++ media/js/src/PromptBlock.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { STATIC_URL } from './utils/utils';
+import PropTypes from 'prop-types';
+
+export const PromptBlock = ({ text }) => {
+    return (
+        <div className="prompt-block">
+            <div className="prompt-gfx">
+                <img className="prompt-img" alt="Reminder:"
+                    src={`${STATIC_URL}img/icon-bell.svg`} />
+            </div>
+            <p className="mb-2">
+                <b>Prompt Block:</b> {text}
+            </p>
+        </div>
+    );
+};
+
+PromptBlock.propTypes = {
+    text: PropTypes.string.isRequired
+};
