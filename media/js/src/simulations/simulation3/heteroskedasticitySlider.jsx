437a8d17b484e32b17dff1390e0b6353772d512a
Zarina Mustapha <zarina@columbia.edu>
2025-03-25 14:58:42 -0400

    Styling of skedasticity slider

--- media/js/src/simulations/simulation3/heteroskedasticitySlider.jsx
+++ media/js/src/simulations/simulation3/heteroskedasticitySlider.jsx
@@ -3,18 +3,28 @@ import PropTypes from 'prop-types';
 
 export const HeteroskedasticitySlider = ({
     heteroskedasticity, setHeteroskedasticity }) => (
-    <div className='mt-3 me-2'>
-        <label className='me-2'>Homoskedasticity</label>
-        <input
-            type="range"
-            min="0"
-            max="10"
-            step="0.1"
-            value={heteroskedasticity}
-            onChange={(e) => setHeteroskedasticity(parseFloat(e.target.value))}
-        />
-        <span>{' Heteroskedasticity'}</span>
-    </div>
+        <div className="slider-end-to-end__box">
+            <label htmlFor="skedasticityscale" className="h2 form-label">
+                Degree of skedasticity
+            </label>
+            <div className="slider-end-to-end__input">
+                <input
+                    id="skedasticityscale"
+                    className="form-range"
+                    type="range"
+                    min="0"
+                    max="10"
+                    step="0.1"
+                    value={heteroskedasticity}
+                    onChange={(e) =>
+                        setHeteroskedasticity(parseFloat(e.target.value))}
+                />
+            </div>
+            <div className="slider-end-to-end__scale">
+                <div className="unit"></div>
+                <div className="unit"></div>
+            </div>
+        </div>
 );
 
 HeteroskedasticitySlider.propTypes = {
