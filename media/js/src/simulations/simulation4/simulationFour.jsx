39a6153726433af67c781b7340d6f1dd236cb4d1
Evan Petersen <emp2221@columbia.edu>
2025-04-16 14:46:48 -0400

    Refined the Learning Objective component for Sim 4

--- media/js/src/simulations/simulation4/simulationFour.jsx
+++ media/js/src/simulations/simulation4/simulationFour.jsx
@@ -10,6 +10,35 @@ export const SimulationFour = () => {
 
     const [stage, setStage] = useState(0);
     const handleStage = (e) => setStage(parseInt(e.target.value));
+    // Each index tracks the state for a different module
+    // ['Polynomials', 'Logarithms', 'Interactions']
+    const [progress, setProgress] = useState([0,0,0]);
+
+    // For the continue/review buttons
+    // eslint-disable-next-line no-unused-vars
+    const handleProgress = (val) => {
+        setProgress(progress.map((x,i) => i === stage ? val : x));
+    };
+
+    const mkModuleBtns = () => ['Polynomials', 'Logarithms', 'Interactions']
+        .map((label, index) => (
+            <button onClick={handleStage} key={index}
+                value={index} className={'btn btn-primary m-1'}
+                disabled={stage===index}>
+                {label}
+            </button>));
+
+    const mkProgressBar = () => <div className="sim-progress">
+        <h4>Your progress:</h4>
+        <ul>
+            {['Learn', 'Apply', 'Assess'].map((word, i) =>
+                <li key={i} className={
+                    `${i > progress[stage] ?
+                        'incomplete' : 'completed'}`}
+                >{word}</li>
+            )}
+        </ul>
+    </div>;
     const [showRegLine, setshowRegLine] = useState(
         [false, false, false, false]);
     const [showDatasets, setshowDatasets] = useState(
@@ -17,36 +46,11 @@ export const SimulationFour = () => {
 
     const polynomialSteps = [
         {
-            // Simulation preamble
             stepNumber: '•',
             segment: 'preamble',
             subtitle: 'Simulation 4',
             title: 'Non-linear Regressions',
-            content: (
-                <>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit
-                    Mollitia doloremque iure explicabo quis asperiores
-                    natus. Inventore laborum tempore, molestias expedita
-                    nemo nostrum dicta vel eum autem laboriosam ad ipsa
-                    modi!
-                    </p>
-                    {['Polynomials', 'Logarithms', 'Interactions'].map(
-                        (label, index) => (
-                            <button onClick={handleStage} key={index}
-                                value={index} className={'btn btn-primary m-1'}
-                                disabled={stage===index}>
-                                {label}
-                            </button>))}
-                </>
-            )
-        },
-        {
-            // Learning goals
-            icon: `${STATIC_URL}/img/icon-goal.svg`,
-            headerId: 'learningObjective',
-            title: 'Learning objectives: Polynomials',
-            content: (
+            content: <>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit
                     Mollitia doloremque iure explicabo quis asperiores
@@ -54,7 +58,23 @@ export const SimulationFour = () => {
                     nemo nostrum dicta vel eum autem laboriosam ad ipsa
                     modi!
                 </p>
-            )
+                {mkModuleBtns()}
+            </>
+        },
+        {
+            icon: `${STATIC_URL}/img/icon-goal.svg`,
+            headerId: 'learningObjective',
+            title: 'Learning objectives: Polynomials',
+            content: <>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    Mollitia doloremque iure explicabo quis asperiores
+                    natus. Inventore laborum tempore, molestias expedita
+                    nemo nostrum dicta vel eum autem laboriosam ad ipsa
+                    modi!
+                </p>
+                {mkProgressBar()}
+            </>
         },
         {
             headerId: 'whatarenonlinearregression',
